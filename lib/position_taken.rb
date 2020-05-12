0b3dc80b7d0c9c182be5d083b33e426e35411d7c
Jason Chilcott <jasonchilcott@me.com>
2020-05-12 22:38:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != (" " || "" || nil)
-    true
-  else
-    false
-  end
+  board[index] != " "
+
 end
